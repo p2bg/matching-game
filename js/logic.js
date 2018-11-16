981d244f9784309d15d1f5f7ff04ccde0c877237
Pedro Paulo B. Grein <pedro.grein@gmail.com>
2018-11-16 16:08:41 -0200

    Add strict mode to javascript. Correct moves count

--- js/logic.js
+++ js/logic.js
@@ -1,5 +1,23 @@
+// Whole-script strict mode syntax
+'use strict';
+
 //avoid click if two cards are selected
 var cardsTurned = false;
+
+// cards
+var order = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+
+//apply random order
+var cards = $('.game-board>div');
+
+//store number of clicks
+var clickCount = 0;
+
+// number of Moves
+var movesNum = 0;
+
+//store number of matchs
+var matchs = 0;
 
 //randomize cards order (shuffle function font: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array)
 function shuffle(array) {
@@ -18,66 +36,55 @@ function shuffle(array) {
     array[randomIndex] = temporaryValue;
   }
   return array;
-	//apply random number to class order
-	each
 }
 
-// cards
-var order = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+//shuffle the cards order
 order = shuffle(order);
 
-//apply random order
-var cards = $('.game-board>div');
-
+//apply random number to class order
 cards.each(function(index){
 	$(this).addClass('order-'+order[index]);
 })
-
-//store number of clicks
-var clickCount = 0;
-//store number of matchs
-var matchs = 0;
 
 //flip card
 $('.game-board').on('click','.flipper',function(){
 	if(!cardsTurned){
 		$(this).toggleClass('rotate');
 		clickCount++;
-		starCount(clickCount);
 		var cardContent = $('.rotate .back')
 			//checks if first or second card
-			if(clickCount%2 ===0){
+			if(clickCount%2 === 0){
 				cardsTurned = true;
-				console.log(cardsTurned);
-				//set the moves
-				$('.moves').text(clickCount/2);
 				//get card content for comparison
-				 if(typeof cardContent[1] !== 'undefined'){
-				var contentOne = cardContent[0].innerHTML;
-				var contentTwo = cardContent[1].innerHTML;
+				if(typeof cardContent[1] !== 'undefined'){
+					var contentOne = cardContent[0].innerHTML;
+					var contentTwo = cardContent[1].innerHTML;
 				//match
-				if(contentOne === contentTwo){
-					setTimeout(function(){
-						$('.rotate').addClass('match animated flash faster');
-						$('.match').removeClass('flipper rotate');
-						matchs++;
-						gameStatus(matchs);
-					}, 600);
-				//mismatch
-				}else{
-					setTimeout(function(){
-						$('.rotate').toggleClass('wrong');
-						setTimeout(function() {
-							$('.rotate').toggleClass('rotate wrong');
-						},600);
-					},300);
-				}
+					if(contentOne === contentTwo){
+						setTimeout(function(){
+							$('.rotate').addClass('match animated flash faster');
+							$('.match').removeClass('flipper rotate');
+							matchs++;
+							gameStatus(matchs);
+						}, 600);
+					//mismatch
+					}else{
+						setTimeout(function(){
+							$('.rotate').toggleClass('wrong');
+							setTimeout(function() {
+								$('.rotate').toggleClass('rotate wrong');
+							},600);
+						},300);
+					}
 			}
 			setTimeout(function() {
 				cardsTurned = false;
-				console.log(cardsTurned);
 			},900);
 		}
+		//set the moves
+		moves();
+		//set the Stars
+		starCount(clickCount);
 	}
 });
 
@@ -111,10 +118,10 @@ function gameStatus(matchs){
 }
 
 //change the number of stars based on number of moves
-function starCount(moves){
-	if(moves===20){
+function starCount(clickCount){
+	if(movesNum===10 && clickCount%2 === 0){
 		$('.stats p i:first-child').remove();
-	}else if(moves===30){
+	}else if(movesNum===15 && clickCount%2 === 0){
 		$('.stats p i:first-child').remove();
 	}
 }
@@ -141,3 +148,11 @@ $(document).ready(function (e) {
 
     setTimeout(update, 1000);
 });
+
+//set the score
+function moves(){
+	if($('.rotate').length===2){
+		movesNum++;
+	$('.moves').text(movesNum);
+	}
+}
